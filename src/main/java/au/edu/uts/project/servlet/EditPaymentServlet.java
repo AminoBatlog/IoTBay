0abba44ea18960b7b69d563b2d6288ee3fa9516f
Christie <98926546+CC0369@users.noreply.github.com>
2022-05-13 20:21:27 +0900

    Update EditPaymentServlet.java

--- src/main/java/au/edu/uts/project/servlet/EditPaymentServlet.java
+++ src/main/java/au/edu/uts/project/servlet/EditPaymentServlet.java
@@ -21,7 +21,7 @@ public class EditPaymentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(true);
-        Integer payment_ID = Integer.parseInt(request.getParameter("payment_ID"));
+        Integer payment_ID = Integer.parseInt(request.getParameter("Payment_ID"));
         PaymentDao paymentdao = (PaymentDao) session.getAttribute("paymentdao");
         Payment payment = null;
         try{
